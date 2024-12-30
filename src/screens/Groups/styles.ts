df0665000c17b81a8ecf2041d69e39fcd0898be9
Alyson Ronnan Martins <devalyson@gmail.com>
2024-12-30 10:27:48 -0300

    feat: add icon

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -5,10 +5,4 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
   align-items: center;
-  justify-content: center;
-`
-
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
 `
