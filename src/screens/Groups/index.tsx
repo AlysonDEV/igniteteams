d994737a602ef13ab7b401d89cad4f7ff8e802ea
Alyson Ronnan Martins <devalyson@gmail.com>
2024-12-30 23:03:34 -0300

    feat: created component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,19 @@
 
-import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
